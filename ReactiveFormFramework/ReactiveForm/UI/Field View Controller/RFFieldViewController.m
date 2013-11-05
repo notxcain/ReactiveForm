f21dafadbe1d957bb607c1c69a6463bf615549e7
Denis Mikhaylov <mda.mymail@gmail.com>
2013-11-05 08:25:28 +0400

    Update RFFieldViewController
    Add lazy view loading

--- ReactiveFormFramework/ReactiveForm/UI/Field View Controller/RFFieldViewController.m
+++ ReactiveFormFramework/ReactiveForm/UI/Field View Controller/RFFieldViewController.m
@@ -8,6 +8,34 @@
 
 #import "RFFieldViewController.h"
 
-@implementation RFFieldViewController
+@interface RFFieldViewController ()
 
+@end
+
+@implementation RFFieldViewController
+@synthesize view = _view;
+- (UITableViewCell *)view
+{
+	if (_view) return _view;
+	
+	[self loadView];
+	[self viewDidLoad];
+	
+	return _view;
+}
+
+- (BOOL)isViewLoaded
+{
+	return (_view != nil);
+}
+
+- (void)viewDidLoad
+{
+	
+}
+
+- (void)loadView
+{
+	self.view = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:NSStringFromClass([self class])];
+}
 @end
