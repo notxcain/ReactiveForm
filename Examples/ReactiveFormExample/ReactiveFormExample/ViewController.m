1ad81d6917950616a6e661d60f17d7f240d9c0de
Alexander Desyatov <a.desyatov@qiwi.ru>
2013-11-06 14:55:43 +0400

    Add example project

---
+++ Examples/ReactiveFormExample/ReactiveFormExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ReactiveFormExample
+//
+//  Created by Alexander Desyatov on 06.11.13.
+//  Copyright (c) 2013 ReactiveForm. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
