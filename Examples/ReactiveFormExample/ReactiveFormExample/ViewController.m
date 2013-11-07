7cd444dc27b6f77ebb677198114727b2fda3bc3e
Alexander Desyatov <a.desyatov@qiwi.ru>
2013-11-07 14:50:23 +0400

    Add example with RFTextField

--- Examples/ReactiveFormExample/ReactiveFormExample/ViewController.m
+++ Examples/ReactiveFormExample/ReactiveFormExample/ViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveForm/RFForm.h>
+#import <ReactiveForm/RFFormTableViewDataSource.h>
+#import <ReactiveForm/RFFormPresentation.h>
+#import <ReactiveForm/RFTextField.h>
+#import <ReactiveForm/RFFieldViewController.h>
+#import <ReactiveForm/RFContainer.h>
 
 @interface ViewController ()
-
+@property(nonatomic,strong,readonly) UITableView *tableView;
+@property(nonatomic,strong,readonly) RFFormTableViewDataSource *dataSourceModel;
 @end
 
 @implementation ViewController
@@ -17,7 +24,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    RFTextField *textField = [RFTextField fieldWithName:@"textField" title:@"title"];
+    RFContainer *container = [RFContainer container];
+    [container addElement:textField];
+    RFFieldController *textFieldController = [[RFFieldController alloc] init];
+    RFFormPresentation *formPresentation = [RFFormPresentation createWithBlock:^(id<RFFormPresentationBuilder> builder) {
+        [builder addMatcher:[RFTextField class] instantiator:^RFFieldController *(RFField *field) {
+            return textFieldController;
+        }];
+    }];
+    
+    RFForm *form = [RFForm form];
+    [form addSectionWithElement:container];
+    _dataSourceModel = [[RFFormTableViewDataSource alloc] initWithForm:form presentation:formPresentation];
+    
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    self.tableView.dataSource = self.dataSourceModel;
+    self.tableView.tableFooterView = [[UIView alloc] init];
+    [self.view addSubview:self.tableView];
 }
 
 - (void)didReceiveMemoryWarning
